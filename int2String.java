fc20ab7211c4b600795450352015db3127532790
AshuJavaCodes <ashutosh.singh19071999@gmail.com>
2021-08-17 02:47:33 +0530

    Integer 2 String HackerRank

---
+++ int2String.java
@@ -0,0 +1,24 @@
+// int2String_HackerRank
+import java.util.Scanner;
+class int2String{
+
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int Number = input.nextInt();
+		if(Number>=-100 && Number <=100)
+			{
+			System.out.println("Good Job");
+			}
+	
+		else
+		{
+			System.out.println("Wrong Answer");
+		}
+
+
+
+
+
+	}
+
+}
