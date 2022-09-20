f27f62f818834dffa6364a074ef34d03eafd7836
RedEyesNCode <ashutosh.singh19071999@gmail.com>
2022-09-20 22:06:35 +0530

    Recursion , How functions work internally in java, Base condition in recursion

--- Java/src/com/redeyesncode/algos/RedRecursion/Recursion.java
+++ Java/src/com/redeyesncode/algos/RedRecursion/Recursion.java
@@ -8,7 +8,19 @@ public class Recursion {
         // Print number from  1 to 5.
         // Conditions are : 1. Call only one function. 2. You cannot modify the function
 
+
         printNumberOne(1);
+
+        // Key points related to recursion are:
+
+        // 1. Functions remain the stack until they have finished  their lines of code.
+        // 2. Once the function have completed their line of code. They are removed from the stack the order they were placed.
+
+        // RECURSION ( A Coder's Nightmare )
+
+        // A function calling itself it is known as recursion.
+        // Every recursion program should have a base condition which holds a boolean value to tell java that "If this particular
+        // condition is true stop the recursive call.
 
     }
 
